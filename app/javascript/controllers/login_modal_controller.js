bc51854618775abfe7182173ed6a6be8cecb48e0
rakib <rakib.hasan.cse.pust@gmail.com>
2025-08-28 22:40:34 +0600

    Add all category sidebar and modify --navbar --all categories --featured product and so on

--- app/javascript/controllers/login_modal_controller.js
+++ app/javascript/controllers/login_modal_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["loginModal","registrationModal"]
   connect() {
-    console.log("Authentication Modal Controller connected!")
+    // console.log("Authentication Modal Controller connected!")
   }
   open(event){
     event.preventDefault()
